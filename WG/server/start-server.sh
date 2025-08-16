5d548f02cafc43ad2451a5e0f0cdba626678349e
Edward Vidal Jr <develone@sbcglobal.net>
2025-08-15 18:44:26 -0600

    initial server scripts

---
+++ WG/server/start-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 'Starting the wireguard sever'
+echo ''
+sudo wg-quick up wg0
+
+ip link
+
---
+++ WG/server/stop-server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 'Stopoing the wireguard sever'
+echo ''
+sudo wg-quick down wg0
+
+ip link
+
